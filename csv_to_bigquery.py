11666900e610ed9b0b6e73f4152df7e8eefa8b14
Mina <minap2001@gmail.com>
2024-06-04 23:08:48 +0200

    done treca skripta

--- csv_to_bigquery.py
+++ csv_to_bigquery.py
@@ -4,20 +4,39 @@ import pandas as pd
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'skladistenje2-558414757286.json'
 client = bigquery.Client()
-project_id = 'ipz-vezba4-421011'
+project_id = 'skladistenje2'
 dataset_id = 'skladistenje2.pillow_readings'
-table_id = "ipz-vezba4-421011.garden_readings.readings"
+table_id = "skladistenje2.pillow_readings.pillow"
 
-folder_path = '/path/to/csv/folder/'
+folder_path = './pillowOutput'
+
+dtype = {
+    '_id': str,
+    'snoringRange': float,
+    'respirationRate': float,
+    'bodyTemperature': float,
+    'limbMovement': float,
+    'bloodOxygen': float,
+    'rem': float,
+    'hoursSleeping': float,
+    'heartRate': float,
+    'stresState': float
+}
 
 for file_name in os.listdir(folder_path):
     if file_name.endswith('.csv'):
         file_path = os.path.join(folder_path, file_name)
-        df = pd.read_csv(file_path,skiprows=1)
-        #df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
+        
+        try:
+            df = pd.read_csv(file_path,dtype=dtype)
+            #df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
 
-        job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+            job_config = bigquery.LoadJobConfig()
+            job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
 
-        client.load_table_from_dataframe(df, table_id, job_config=job_config)
-        print(f"Data loaded from {file_name} into {project_id}.{dataset_id}.{table_id}")
+            client.load_table_from_dataframe(df, table_id, job_config=job_config)
+            print(f"Data loaded from {file_name} into {project_id}.{dataset_id}.{table_id}")
+        except pd.errors.EmptyDataError:
+            print(f"The file {file_path} is empty.")
+        except Exception as e:
+            print(f"Error loading data from {file_name}: {e}")
